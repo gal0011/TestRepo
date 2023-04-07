c4a665e18427c5da6aeeb7897656de15f19baf2d
gal0011 <gal0011@mix.wvu.edu>
2023-04-06 23:36:55 -0400

    Updated testing for query

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import {txtDate, txtOdometer, btnOdoSubmit} from "./ui";
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc, getDoc,arrayUnion, arrayRemove, updateDoc } from "firebase/firestore";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { getFirestore, doc, setDoc, getDoc,arrayUnion, arrayRemove, updateDoc, collection, query, where, getDocs } from "firebase/firestore";
+//import { collection, query, where, getDocs } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
